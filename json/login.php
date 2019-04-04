f4d7375ffed9fa99fe5291f0e50d97bb79c87ce1
mariibecadh <mariibeca2012@gmail.com>
2019-04-04 12:03:45 -0300

    clase Jaon

---
+++ json/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+
+if ($_POST) {
+  $email = $_POST['email']
+}
+ ?>
+
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
